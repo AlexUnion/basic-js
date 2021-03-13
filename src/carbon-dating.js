50d565995e825a4b8457fe2e8401e5252970a6fb
Al_chemick <alex2015mig@gmail.com>
2021-03-13 22:13:44 +0200

    feat: realize carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,18 @@
-const CustomError = require("../extensions/custom-error");
-
-const MODERN_ACTIVITY= 15; 
+const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
+const LN_BY_TWO = 0.693;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivityStr) {
+
+  if (typeof sampleActivityStr !== 'string') return false;
+
+  const sampleActivity = Number(sampleActivityStr);
+
+  if (Number.isNaN(sampleActivity) || sampleActivity <= 0 || sampleActivity > MODERN_ACTIVITY) return false;
+
+  const numberActivity = MODERN_ACTIVITY / sampleActivity; // N0 / N
+  const lnN = Math.log(numberActivity); // ln( N0 / N)
+  const k = LN_BY_TWO / HALF_LIFE_PERIOD;
+  const time = lnN / k;
+  return Math.ceil(time);
 };
