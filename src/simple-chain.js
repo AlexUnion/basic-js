377a02f3849e1b82282f6ce661458840c565e9e4
Al_chemick <alex2015mig@gmail.com>
2021-03-29 19:37:20 +0300

    feat: add simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,31 @@
-const CustomError = require("../extensions/custom-error");
-
-const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+function ChainMaker() {
+  this._arr = [];
+  this.getLength = function () {
+    return this._arr.length;
+  };
+  this.addLink = function (value) {
+    this._arr.push(
+        `( ${value} )`
+    );
+    return this;
+  };
+  this.removeLink = function(position) {
+    if (position < 1 || position > this._arr.length || Number.isNaN(position)) {
+      this._arr.length = 0;
+      throw new Error();
+    }
+    this._arr.splice(position - 1, 1);
+    return this;
+  };
+  this.reverseChain = function() {
+    this._arr.reverse();
+    return this;
+  };
+  this.finishChain = function () {
+    const res = this._arr.join('~~');
+    this._arr.length = 0;
+    return res;
   }
-};
+}
 
-module.exports = chainMaker;
+module.exports = new ChainMaker();
